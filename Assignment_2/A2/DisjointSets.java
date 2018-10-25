91e9ff02f60de57bcf5d7327252b6c83daaec067
atg-abhijay <atg.abhijay@gmail.com>
2018-10-25 19:18:48 -0400

    A2.1 - uncommented union operations. done with q1

--- Assignment_2/A2/DisjointSets.java
+++ Assignment_2/A2/DisjointSets.java
@@ -96,24 +96,24 @@ public class DisjointSets {
 
         DisjointSets myset = new DisjointSets(6);
         System.out.println(myset);
-        // System.out.println("-> Union 2 and 3");
-        // myset.union(2,3);
-        // System.out.println(myset);
-        // System.out.println("-> Union 2 and 3");
-        // myset.union(2,3);
-        // System.out.println(myset);
-        // System.out.println("-> Union 2 and 1");
-        // myset.union(2,1);
-        // System.out.println(myset);
-        // System.out.println("-> Union 4 and 5");
-        // myset.union(4,5);
-        // System.out.println(myset);
-        // System.out.println("-> Union 3 and 1");
-        // myset.union(3,1);
-        // System.out.println(myset);
-        // System.out.println("-> Union 2 and 4");
-        // myset.union(2,4);
-        // System.out.println(myset);
+        System.out.println("-> Union 2 and 3");
+        myset.union(2,3);
+        System.out.println(myset);
+        System.out.println("-> Union 2 and 3");
+        myset.union(2,3);
+        System.out.println(myset);
+        System.out.println("-> Union 2 and 1");
+        myset.union(2,1);
+        System.out.println(myset);
+        System.out.println("-> Union 4 and 5");
+        myset.union(4,5);
+        System.out.println(myset);
+        System.out.println("-> Union 3 and 1");
+        myset.union(3,1);
+        System.out.println(myset);
+        System.out.println("-> Union 2 and 4");
+        myset.union(2,4);
+        System.out.println(myset);
 
     }
 
