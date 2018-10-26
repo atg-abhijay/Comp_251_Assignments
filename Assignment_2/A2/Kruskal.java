234f1c8fc7d2a4a252b441a9324f94af3ea5ec84
atg-abhijay <atg.abhijay@gmail.com>
2018-10-26 14:40:06 -0400

    A2.2 - completed it

--- Assignment_2/A2/Kruskal.java
+++ Assignment_2/A2/Kruskal.java
@@ -6,15 +6,34 @@ public class Kruskal{
     public static WGraph kruskal(WGraph g){
 
         /* Fill this method (The statement return null is here only to compile) */
-        
-        return null;
+        WGraph mst = new WGraph();
+        /**
+         * start with each vertex
+         * in its own component
+         */
+        DisjointSets p = new DisjointSets(g.getNbNodes());
+        ArrayList<Edge> sortedEdges = g.listOfEdgesSorted();
+        for(Edge e: sortedEdges) {
+            Boolean safeToAdd = IsSafe(p, e);
+            if(safeToAdd) {
+                mst.addEdge(e);
+            }
+        }
+        return mst;
     }
 
     public static Boolean IsSafe(DisjointSets p, Edge e){
+        int u = e.nodes[0];
+        int v = e.nodes[1];
 
+        if(p.find(u) != p.find(v)) {
+            p.union(u, v);
+            return true;
+        }
+        else {
+            return false;
+        }
         /* Fill this method (The statement return 0 is here only to compile) */
-        return true;
-    
     }
 
     public static void main(String[] args){
@@ -24,5 +43,5 @@ public class Kruskal{
         WGraph t = kruskal(g);
         System.out.println(t);
 
-   } 
+   }
 }
