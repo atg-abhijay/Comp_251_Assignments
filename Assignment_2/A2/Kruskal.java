4caf43c13769b0c9c7dec98f3f54f1bbedafc40d
atg-abhijay <atg.abhijay@gmail.com>
2018-10-26 20:59:04 -0400

    A2.2 - added comments

--- Assignment_2/A2/Kruskal.java
+++ Assignment_2/A2/Kruskal.java
@@ -12,6 +12,11 @@ public class Kruskal{
          * in its own component
          */
         DisjointSets p = new DisjointSets(g.getNbNodes());
+        /**
+         * obtain the edges in increasing
+         * order of their weights and check
+         * if they are safe to add to the MST
+         */
         ArrayList<Edge> sortedEdges = g.listOfEdgesSorted();
         for(Edge e: sortedEdges) {
             Boolean safeToAdd = IsSafe(p, e);
@@ -26,6 +31,14 @@ public class Kruskal{
         int u = e.nodes[0];
         int v = e.nodes[1];
 
+        /**
+         * if the rep of u is not the
+         * same as rep of v, then they
+         * belong in different components.
+         * therefore, it is safe to add the
+         * edge uv. merge the components
+         * containing u and v and return true
+         */
         if(p.find(u) != p.find(v)) {
             p.union(u, v);
             return true;
