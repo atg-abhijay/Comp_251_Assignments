3f1bb82429779966bbf311a5a5b2eb2e97846548
atg-abhijay <atg.abhijay@gmail.com>
2018-11-30 01:23:54 -0500

    A5 - got solution for Balloons question. might have bad runtime though

--- Assignment_5/balloon.java
+++ Assignment_5/balloon.java
@@ -4,7 +4,8 @@ import java.util.LinkedList;
 
 public class balloon {
     public static void main(String[] args) {
-        File inputFile = new File("HW5/test.txt"); //testBalloons
+        final int largeValue = (int) Math.pow(10, 8);
+        File inputFile = new File("HW5/testBalloons.txt"); //
         Scanner sc = null;
         try{
             sc = new Scanner(inputFile);
@@ -16,7 +17,7 @@ public class balloon {
 
         int numProblems = sc.nextInt();
         int[] problemSizes = new int[numProblems];
-        int[] numBalloonsReq = new int[numProblems];
+        int[] numArrowsReq = new int[numProblems];
 
         /**
          * reading in the
@@ -28,20 +29,53 @@ public class balloon {
 
         for(int i = 0; i < problemSizes.length; i++) {
             int problemSize = problemSizes[i];
-            LinkedList<Integer> balloonPositions = new LinkedList<Integer>();
+            int[] balloonPositions = new int[problemSize];
             // System.out.println(problemSize);
             /**
              * reading in the balloon
              * positions for a specific problem
              */
-            int numBalloons = 0;
-            int currentBalloonHeight = 0;
             for(int j = 0; j < problemSize; j++) {
-                balloonPositions.add(sc.nextInt());
+                balloonPositions[j] = sc.nextInt();
                 // System.out.print(balloonPositions[j] + " ");
             }
-            // System.out.println();
 
+            int currentArrowHeight = balloonPositions[0]-1;
+            balloonPositions[0] = largeValue;
+            int numBalloonsShot = 1;
+            int index = 1;
+            boolean firstJump = false;
+            int startIdxNextIter = -1;
+            int numArrows = 0;
+            while(numBalloonsShot < problemSize) {
+                if(currentArrowHeight == balloonPositions[index]) {
+                    balloonPositions[index] = largeValue;
+                    numBalloonsShot++;
+                    currentArrowHeight--;
+                }
+
+                else{
+                    if(!firstJump && balloonPositions[index] != largeValue) {
+                        startIdxNextIter = index;
+                        firstJump = true;
+                    }
+                }
+                index++;
+
+                if(numBalloonsShot == problemSize) {
+                    numArrows++;
+                    break;
+                }
+
+                if(index == problemSize || currentArrowHeight == 0) {
+                    numArrows++;
+                    index = startIdxNextIter;
+                    currentArrowHeight = balloonPositions[index];
+                    firstJump = false;
+                }
+            }
+            numArrowsReq[i] = numArrows;
+            System.out.println(numArrows);
         }
     }
 }
