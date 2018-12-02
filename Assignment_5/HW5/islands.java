632a82b38421ef9dc53aefd0fd337ec30b206a88
atg-abhijay <atg.abhijay@gmail.com>
2018-12-02 18:32:00 -0500

    A5 - Islands: added comments

--- Assignment_5/HW5/islands.java
+++ Assignment_5/HW5/islands.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 import java.io.*;
 
 class Node {
+    /**
+     * belongToIsland -
+     * which island number does the 'cell' belong
+     * to. 0 at the beginning for every cell.
+     *
+     * landOrWater -
+     * 1 if 'cell' is land, 0 for water
+     *
+     */
     public boolean visited;
     public int belongToIsland;
     public int landOrWater;
@@ -31,8 +40,24 @@ public class islands {
         for(int i = 0; i < numProblems; i++) {
             int numRows = sc.nextInt();
             int numColumns = sc.nextInt();
+            /**
+             * creating a larger array wherein we
+             * surround the given array with water
+             * on all sides so that we can handle
+             * the corner or edge cells without
+             * having to worry about going out of bounds
+             *
+             * new array looks like this -
+             *      #########
+             *      # given #
+             *      # array #
+             *      #########
+             */
             Node[][] ocean = new Node[numRows+2][numColumns+2];
 
+            /**
+             * making the extra water part
+             */
             for(int k = 0; k < numColumns+2; k++) {
                 ocean[0][k] = new Node(0);
                 ocean[numRows+1][k] = new Node(0);
@@ -59,9 +84,18 @@ public class islands {
             }
 
             int numIslands = 0;
+            /**
+             * even though we have an array larger
+             * than the original one, we only iterate
+             * over those cells in the original array
+             */
             for(int j = 0; j < numRows; j++) {
                 for(int k = 0; k < numColumns; k++) {
                     Node cell = ocean[j+1][k+1];
+                    /**
+                     * if the cell is a piece of land
+                     * and it does not belong to any island
+                     */
                     if(cell.landOrWater == 1 && cell.belongToIsland == 0) {
                         numIslands++;
                         cell.belongToIsland = numIslands;
@@ -89,6 +123,16 @@ public class islands {
     }
 
 
+    /**
+     * Recursively mark the neighbors of the
+     * original cell with the island number
+     * of the original cell
+     *
+     * @param ocean - 2D array of Node
+     * @param row - row number of original cell
+     * @param column - column number of original cell
+     * @param original - cell whose neighbors we wish to mark
+     */
     public static void markNeighbors(Node[][] ocean, int row, int column, Node original) {
         Node above = ocean[row-1][column];
         Node toLeft = ocean[row][column-1];
@@ -112,6 +156,18 @@ public class islands {
         }
     }
 
+    /**
+     * If the neighboring cell to the parent is
+     * a piece of land and it has not been visited
+     * yet, we set the neighbor's island number
+     * to that of the parent and set its visit
+     * status to true
+     *
+     * @param parent
+     * @param neighbor
+     * @return true if we can recursively iterate over
+     * the neighbors of this neighbor, false otherwise
+     */
     public static boolean evaluateNeighbor(Node parent, Node neighbor) {
         if(neighbor.landOrWater == 1 && !neighbor.visited) {
             neighbor.belongToIsland = parent.belongToIsland;
