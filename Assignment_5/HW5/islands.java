da6cb2dd548d44cc66fcc03a4d58876adbcae3f6
atg-abhijay <atg.abhijay@gmail.com>
2018-12-02 17:32:49 -0500

    A5 - islands solution is working

--- Assignment_5/HW5/islands.java
+++ Assignment_5/HW5/islands.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
-
-import jdk.nashorn.internal.codegen.types.NumericType;
-
 import java.io.*;
+
+class Node {
+    public boolean visited;
+    public int belongToIsland;
+    public int landOrWater;
+
+    Node(int lw) {
+        this.visited = false;
+        this.belongToIsland = 0;
+        this.landOrWater = lw;
+    }
+}
 
 public class islands {
     // final int largeValue = (int) Math.pow(10, 8);
@@ -22,8 +31,18 @@ public class islands {
         for(int i = 0; i < numProblems; i++) {
             int numRows = sc.nextInt();
             int numColumns = sc.nextInt();
+            Node[][] ocean = new Node[numRows+2][numColumns+2];
 
-            int[][][] ocean = new int[numRows+2][numColumns+2][2];
+            for(int k = 0; k < numColumns+2; k++) {
+                ocean[0][k] = new Node(0);
+                ocean[numRows+1][k] = new Node(0);
+            }
+
+            for(int j = 0; j < numRows+2; j++) {
+                ocean[j][0] = new Node(0);
+                ocean[j][numColumns+1] = new Node(0);
+            }
+
             /**
              * reading in the data
              */
@@ -31,25 +50,62 @@ public class islands {
                 String row = sc.next();
                 for(int k = 0; k < numColumns; k++) {
                     char cell = row.charAt(k);
+                    int landOrWater = 0;
                     if(cell == '-') {
-                        ocean[j+1][k+1][0] = 1;
+                        landOrWater = 1;
                     }
-                    // ocean[j+1][k+1]
+                    ocean[j+1][k+1] = new Node(landOrWater);
                 }
             }
 
             int numIslands = 0;
             for(int j = 0; j < numRows; j++) {
                 for(int k = 0; k < numColumns; k++) {
-                    int[] cell = ocean[j+1][k+1];
-                    if(cell[0] == 1 && cell[1] == 0) {
-                        cell[1] = checkSurroundings(ocean, j+1, k+1);
+                    Node cell = ocean[j+1][k+1];
+                    if(cell.landOrWater == 1 && cell.belongToIsland == 0) {
+                        numIslands++;
+                        cell.belongToIsland = numIslands;
+                        cell.visited = true;
+                        markNeighbors(ocean, j+1, k+1, cell);
                     }
                 }
             }
-            // printArray(ocean);
+            System.out.println(numIslands);
         }
     }
+
+
+    public static void markNeighbors(Node[][] ocean, int row, int column, Node original) {
+        Node above = ocean[row-1][column];
+        Node toLeft = ocean[row][column-1];
+        Node below = ocean[row+1][column];
+        Node toRight = ocean[row][column+1];
+
+        if(above.landOrWater == 1 && !above.visited) {
+            above.belongToIsland = original.belongToIsland;
+            above.visited = true;
+            markNeighbors(ocean, row-1, column, above);
+        }
+
+        if(toLeft.landOrWater == 1 && !toLeft.visited) {
+            toLeft.belongToIsland = original.belongToIsland;
+            toLeft.visited = true;
+            markNeighbors(ocean, row, column-1, toLeft);
+        }
+
+        if(below.landOrWater == 1 && !below.visited) {
+            below.belongToIsland = original.belongToIsland;
+            below.visited = true;
+            markNeighbors(ocean, row+1, column, below);
+        }
+
+        if(toRight.landOrWater == 1 && !toRight.visited) {
+            toRight.belongToIsland = original.belongToIsland;
+            toRight.visited = true;
+            markNeighbors(ocean, row, column+1, toRight);
+        }
+    }
+
 
     public static int checkSurroundings(int[][][] ocean, int row, int column) {
         int max = 1;
